0696a2401f9b378c2d39c689c7d02a753e0e995a
FlyLing <963452757@qq.com>
2021-09-30 14:03:31 +0800

    试验

---
+++ Assets/SerializedReference/Scriptes/Debugger.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class Info
+{
+    public int m_Tid;
+    public ulong m_Uid;
+}
+
+[Serializable]
+public class TestInfo : Info
+{
+    public Vector2 m_Test;
+}
+
+[Serializable]
+public class Shell
+{
+    [SerializeReference]
+    public Info m_Info;
+
+    public Shell()
+    {
+        m_Info = new TestInfo();
+    }
+}
+
+
+[Serializable]
+public class Shape
+{
+    public Vector3 m_Position;
+
+    //public Shell[] m_Shells;
+
+    [SerializeReference]
+    public List<Info> infos = new List<Info>() { new TestInfo() }; 
+}
+
+[Serializable]
+public class Box : Shape
+{
+    public Vector3 m_Elur;
+    public Vector3 m_Scale;
+}
+
+[Serializable]
+
+public class Sphere : Shape
+{
+    public float m_Radius;
+}
+
+
+
+public class Debugger : MonoBehaviour
+{
+    [SerializeReference]
+    public Shape m_Shape = new Box();
+}
---
+++ Assets/ShowPrefabInEditor/BaseShell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class BaseShell : MonoBehaviour
+{
+    public GameObject m_Prefab;
+
+    private void OnDisable()
+    {
+        var obj = UnityEditor.PrefabUtility.GetCorrespondingObjectFromOriginalSource<GameObject>(this.gameObject);
+        //var obj = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>(path);
+        if (obj != null)
+        {
+            int childCount = obj.transform.childCount;
+            for (int i = 0; i < childCount; i++)
+            {
+                var child = obj.transform.GetChild(0);
+                GameObject.DestroyImmediate(child.gameObject, true);
+            }
+        }
+    }
+}
